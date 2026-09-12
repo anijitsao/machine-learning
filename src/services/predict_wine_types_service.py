5a2bfe3ffdb12e33540fecfbad495f5d41eef297
Anijit Sau <anijitsau@gmail.com>
2026-09-12 13:54:21 +0000

    prediction route is complete for classification

--- src/services/predict_wine_types_service.py
+++ src/services/predict_wine_types_service.py
@@ -1,13 +1,24 @@
 import numpy as np
+import pandas as pd
 from fastapi import HTTPException, Request
+
+from src.models import PredictWineModel
 
 
 def predict_wine_type(payload, req: Request):
     try:
-        payload_reshaped = np.array(payload).reshape(-1,1)
+        # conversion to dictinary and modifying attributes
+        payload_dict = payload.dict()
+        payload_dict["od280/od315_of_diluted_wines"] = payload_dict["od315_of_diluted_wines"]
+        del payload_dict["od315_of_diluted_wines"]
+
+        # convert to pandas dataframe. Othewise feature names will be missed and ValueError
+        payload_reshaped = pd.DataFrame([payload_dict])
         predictions = req.app.state.ml_models["classification"].predict(payload_reshaped)
-        print("predictions", predictions)
-        return {"wine_type": 2}
+        
+        # convert the predictions to serialize for JSON object
+        predicted_type = int(predictions[0])
+        return {"wine_type": predicted_type}
     except Exception as e:  # noqa: BLE001
         print("Error occurred while getting predictions", e)
         return HTTPException(500, e)    
